9506e1156b524fbebc68cb2d0929858745aa11b9
daniel <picisdaniel2016@gmail.com>
2025-05-20 16:26:47 -0400

    avances con la creacion del hero.jsx

---
+++ src/components/Hero.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { ReactTyped } from "react-typed";
+
+function Hero() {
+  return (
+    <div className="text-white">
+      <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+        <p className="text-[#00df9a]">GROWING WITH DATA ANALYTICS</p>
+        <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">
+          Grow with data.
+        </h1>
+        <div className="flex ">
+          <p className="md:text-5xl sm:text-4xl text-xl font-bold">
+            Fast, flexible financing for
+          </p>
+          <ReactTyped
+            className="md:text-5xl sm:text-4xl text-xl font-bold pl-2"
+            strings={["BTB", "BTC", "SASS"]}
+            typeSpeed={120}
+            backSpeed={140}
+            loop
+          />
+        </div>
+        <p className="md:text-2xl text-xl font-bold text-gray-500">
+          Monitor your data analytics to incrase revenue for BTB, BTC, & SASS
+          platforms.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
